7d9ebb3df5b14e7416d15af2e33f88baa8d9a45c
jbresky <joaquin_bresky@hotmail.com>
2024-02-12 17:55:07 -0300

    using session

--- components/header.tsx
+++ components/header.tsx
@@ -1,17 +1,32 @@
 import Image from "next/image";
 import Link from "next/link";
+import { GiAbstract013 } from "react-icons/gi";
+import Logout from "./logout/logout-form";
 
-const Header = () => {
+const Header = async ({ session }: { session?: any }) => {
+
     return (
         <header className="flex m-auto items-center justify-between">
-            <Link href='/' className="text-lg sm:text-4xl font-mono">
-                Wordyling
+            <Link href='/' className="text-lg flex items-center sm:text-3xl">
+                W
+                <GiAbstract013 />
+                RDYLING
             </Link>
-            <ul className="flex items-center gap-6">
-                <Link href='/my-words' className="hidden sm:block font-medium hover:underline">
-                    My words
-                </Link>
-                <Image alt="belgium" width={50} height={50} src="https://flagsapi.com/DK/shiny/64.png" />
+            <ul className="flex items-center gap-2 sm:gap-6 max-sm:text-sm">
+                {session ? (
+                    <>
+                        <Link href='/my-words' className="hidden sm:block hover:underline">
+                            My words
+                        </Link>
+                        <Logout />
+                        <Image alt="belgium" width={50} height={50} src="https://flagsapi.com/DK/shiny/64.png" />
+                    </>
+                ) : (
+                    <>
+                        <Link href='/login'>Login</Link>
+                        <Link href='/register'>Register</Link>
+                    </>
+                )}
             </ul>
         </header>
     );
