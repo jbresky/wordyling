379a94f93967f4cbf3dfbac513ed8876490fced3
jbresky <joaquin_bresky@hotmail.com>
2024-02-13 20:14:16 -0300

    word container

---
+++ app/my-words/components/word-container.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import { TrashIcon } from "@radix-ui/react-icons";
+import { deleteWord } from "@/server/actions/create-word"
+import { AnimatePresence, motion } from "framer-motion"
+
+const WordContainer = ({ word }: { word: Word }) => {
+    return (
+        <AnimatePresence presenceAffectsLayout>
+            <motion.div
+                layout
+                animate={{ opacity: 1 }}
+                initial={{ opacity: 0 }}
+                exit={{ opacity: 0 }}
+                key={word.id}
+            >
+                <HoverCard key={word.id}>
+                    <HoverCardTrigger>
+                        <p key={word.id} className="text-center text-sm cursor-pointer w-[90px] py-1 font-medium border-b border-black"
+                        >
+                            {word.word}
+                        </p>
+                        <HoverCardContent className="flex flex-col gap-2">
+                            <div className="flex items-center gap-2">
+                                <p>{word.nativeWord}</p>
+                                <p className="italic text-[13px] text-gray-700">
+                                    {word.pronunciation ? `[ ${word.pronunciation} ]` : null}
+                                </p>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <p className="text-[13px] text-gray-400">{word.category}</p>
+                                <TrashIcon className="cursor-pointer" onClick={() => deleteWord({ id: word.id })} color="red" />
+                            </div>
+                        </HoverCardContent>
+                    </HoverCardTrigger>
+                </HoverCard>
+            </motion.div>
+        </AnimatePresence>
+    )
+}
+
+export default WordContainer;
--- app/my-words/components/words.tsx
+++ app/my-words/components/words.tsx
@@ -1,51 +1,25 @@
-'use client'
+import WordContainer from "./word-container";
+import { fetchAllWords } from "@/server/actions/create-word";
 
-import {
-    HoverCard,
-    HoverCardContent,
-    HoverCardTrigger,
-} from "@/components/ui/hover-card"
-import { TrashIcon } from "@radix-ui/react-icons";
-import { deleteWord } from "@/server/actions/create-word"
-import { AnimatePresence, motion } from "framer-motion"
+interface Filters {
+    query: string,
+    filter: string,
+    language: number
+}
 
-const Words = ({ words }: { words: Word[] }) => {
+const Words = async ({ query, filter, language }: Filters) => {
+    const words = await fetchAllWords(query, filter, language)
+
     return (
         <>
-            <h3 className="text-gray-700 max-sm:text-sm">{words.length < 1 ? 'You have no words saved yet!' : 'Hover to see translations and more'}</h3>
-            <section className="flex flex-wrap justify-center md:grid grid-cols-7 gap-6">
-
+            <div className="flex items-center justify-between">
+                <h3 className="text-gray-700 max-sm:text-sm">
+                    {words.length < 1 ? 'No words found' : 'Hover to see translations and more'}
+                </h3>
+            </div>
+            <section className="flex flex-wrap items-center gap-8 md:gap-12">
                 {words?.map((word: Word) => (
-                    <AnimatePresence presenceAffectsLayout>
-                        <motion.div
-                            layout
-                            animate={{ opacity: 1 }}
-                            initial={{ opacity: 0 }}
-                            exit={{ opacity: 0 }}
-                            key={word.id}
-                        >
-                            <HoverCard key={word.id}>
-                                <HoverCardTrigger>
-                                    <p key={word.id} className="text-center text-sm cursor-pointer w-[90px] py-1 font-medium border-b border-black"
-                                    >
-                                        {word.word}
-                                    </p>
-                                    <HoverCardContent className="flex flex-col gap-2">
-                                        <div className="flex items-center gap-2">
-                                            <p>{word.nativeWord}</p>
-                                            <p className="italic text-[13px] text-gray-700">
-                                                {word.pronunciation ? `[ ${word.pronunciation} ]` : null}
-                                            </p>
-                                        </div>
-                                        <div className="flex items-center justify-between">
-                                            <p className="text-[13px] text-gray-400">{word.category}</p>
-                                            <TrashIcon className="cursor-pointer" onClick={() => deleteWord({ id: word.id })} color="red" />
-                                        </div>
-                                    </HoverCardContent>
-                                </HoverCardTrigger>
-                            </HoverCard>
-                        </motion.div>
-                    </AnimatePresence>
+                    <WordContainer key={word.id} word={word} />
                 ))}
             </section>
         </>
