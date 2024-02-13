bf61570ce754b8786d4f07deea93561ec6d7eec2
jbresky <joaquin_bresky@hotmail.com>
2024-02-13 20:10:23 -0300

    handling conditional data

--- components/words/last-words-skeleton.tsx
+++ components/words/last-words-skeleton.tsx
@@ -2,12 +2,12 @@ import { Skeleton } from "../ui/skeleton";
 
 const LastWordsSkeleton = () => {
     return (
-        <div className="flex items-center space-x-4">
-                <Skeleton className="h-4 w-[150px]" />
-                <Skeleton className="h-4 w-[150px]" />
-                <Skeleton className="h-4 w-[150px]" />
-                <Skeleton className="h-4 w-[150px]" />
-                <Skeleton className="h-4 w-[150px]" />
+        <div className="grid grid-cols-7 gap-6 mt-8">
+                {/* @ts-expect-error */}
+              {[...Array(20).keys()].map((_, index) => ( 
+                  <Skeleton key={index} className="h-2 w-[90px]" />
+              ))}
+              
         </div>
     );
 }
--- components/words/last-words.tsx
+++ components/words/last-words.tsx
@@ -1,26 +1,25 @@
 import { fetchLastWords } from "@/server/actions/create-word"
 import Link from "next/link"
-import { Suspense } from "react"
 import LastsContainer from "./last-container"
-import LastWordsSkeleton from "./last-words-skeleton"
 
-export default async function LastWords() {
-    const words = await fetchLastWords()
+export default async function LastWords({ language }: { language: number }) {
+    const words = await fetchLastWords(language)
+
     return (
         <>
             {words.length > 0 ? (
                 <Link href='/my-words'>
-                    <section className="p-4 rounded-2xl bg-indigo-100/90 hover:bg-indigo-100/60 transition duration-300">
+                    <section className="p-4 rounded-2xl bg-sky-100/90 hover:bg-indigo-100/60 transition duration-300">
                         <div className="flex flex-col w-full gap-4">
                             <h1 className="text-xl">Last Words</h1>
-                            <Suspense fallback={<LastWordsSkeleton />}>
                                 <LastsContainer words={words} />
-                            </Suspense>
                         </div>
                     </section>
                 </Link>
             ) : (
-                null
+                <section className="p-4 rounded-2xl bg-sky-100/90 hover:bg-indigo-100/60 transition duration-300">
+                    <p className="font-medium">No words found</p>
+                </section>
             )}
         </>
     )
