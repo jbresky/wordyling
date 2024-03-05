5fcbd24f7dd1f791169cf9117f66a31e97d2442c
jbresky <joaquin_bresky@hotmail.com>
2024-03-05 23:56:55 +0100

    boolean prop for sentence

--- app/my-words/components/word-container.tsx
+++ app/my-words/components/word-container.tsx
@@ -5,6 +5,7 @@ import {
     HoverCardContent,
     HoverCardTrigger,
 } from "@/components/ui/hover-card"
+import { PiDotsThreeBold } from "react-icons/pi";
 import { TrashIcon } from "@radix-ui/react-icons";
 import { deleteWord } from "@/server/actions/create-word"
 import { AnimatePresence, motion } from "framer-motion"
@@ -13,8 +14,9 @@ import {
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover"
+import SentenceDialogForm from "@/components/forms/sentence-form";
 
-const WordContainer = ({ word }: { word: Word }) => {
+const WordContainer = ({ word, isSentence, language }: { word: Word, isSentence?: boolean, language: number }) => {
     return (
         <AnimatePresence presenceAffectsLayout>
             <motion.div
@@ -25,42 +27,55 @@ const WordContainer = ({ word }: { word: Word }) => {
                 key={word.id}
             >
                 {/* mobile popover */}
-                <Popover key={word.id}>
-                    <PopoverTrigger className="md:hidden text-center text-sm cursor-pointer w-[90px] py-1 font-medium border-b border-black">{word.word}</PopoverTrigger>
-                    <PopoverContent>
-                        <div className="flex items-center gap-2">
-                            <p>{word.nativeWord}</p>
-                            <p className="italic tracking-wide text-[13px] text-gray-700">
-                                {word.pronunciation ? `[ ${word.pronunciation} ]` : null}
-                            </p>
-                        </div>
-                        <div className="flex items-center justify-between">
-                            <p className="text-[13px] text-gray-400">{word.category}</p>
-                            <TrashIcon className="cursor-pointer" onClick={() => deleteWord({ id: word.id })} color="red" />
-                        </div>
-                    </PopoverContent>
-                </Popover>
-                
-                <HoverCard key={word.id}>
-                    <HoverCardTrigger>
-                        <p key={word.id} className="hidden md:block text-center text-sm cursor-pointer w-[90px] py-1 font-medium border-b border-black"
-                        >
-                            {word.word}
-                        </p>
-                        <HoverCardContent className="flex flex-col gap-2">
-                            <div className="flex items-center gap-2">
-                                <p>{word.nativeWord}</p>
-                                <p className="italic tracking-wide text-[13px] text-gray-700">
-                                    {word.pronunciation ? `[ ${word.pronunciation} ]` : null}
+                {isSentence ? (
+                    <SentenceDialogForm languageId={language} word={word} />
+                ) : (
+                    <>
+                        <Popover key={word.id}>
+                            <PopoverTrigger className="md:hidden text-center text-sm cursor-pointer w-[90px] py-1 font-medium border-b border-black">
+                                {word.word}
+                            </PopoverTrigger>
+                            <PopoverContent>
+                                <div className="flex justify-between items-center">
+                                    <div className="flex items-center gap-2">
+                                        <p>{word.nativeWord}</p>
+                                        <p className="italic tracking-wide text-[13px] text-gray-700">
+                                            {word.pronunciation ? `[ ${word.pronunciation} ]` : null}
+                                        </p>
+                                    </div>
+                                    <PiDotsThreeBold />
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <p className="text-[13px] text-gray-400">{word.category}</p>
+                                    <TrashIcon className="cursor-pointer" onClick={() => deleteWord({ id: word.id })} color="red" />
+                                </div>
+                            </PopoverContent>
+                        </Popover>
+
+                        <HoverCard key={word.id}>
+                            <HoverCardTrigger>
+                                <p key={word.id} className="hidden md:block text-center text-sm cursor-pointer w-[90px] py-1 font-medium border-b border-black"
+                                >
+                                    {word.word}
                                 </p>
-                            </div>
-                            <div className="flex items-center justify-between">
-                                <p className="text-[13px] text-gray-400">{word.category}</p>
-                                <TrashIcon className="cursor-pointer" onClick={() => deleteWord({ id: word.id })} color="red" />
-                            </div>
-                        </HoverCardContent>
-                    </HoverCardTrigger>
-                </HoverCard>
+                                <HoverCardContent className="flex flex-col gap-2">
+                                    <div className="flex items-center gap-2">
+                                        <p>{word.nativeWord}</p>
+                                        <p className="italic tracking-wide text-[13px] text-gray-700">
+                                            {word.pronunciation ? `[ ${word.pronunciation} ]` : null}
+                                        </p>
+                                    </div>
+                                    <div className="flex items-center justify-between">
+                                        <p className="text-[13px] text-gray-400">{word.category}</p>
+                                        <TrashIcon className="cursor-pointer" onClick={() => deleteWord({ id: word.id })} color="red" />
+                                    </div>
+                                </HoverCardContent>
+                            </HoverCardTrigger>
+                        </HoverCard>
+                    </>
+                )}
+
+
             </motion.div>
         </AnimatePresence>
     )
