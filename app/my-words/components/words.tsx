255fd11d7538c7c592fe83bedbf1741768cf80f8
jbresky <joaquin_bresky@hotmail.com>
2024-03-05 23:57:24 +0100

    sentence dialog

--- app/my-words/components/words.tsx
+++ app/my-words/components/words.tsx
@@ -4,27 +4,33 @@ import { fetchAllWords } from "@/server/actions/create-word";
 interface Filters {
     query: string,
     filter: string,
-    language: number
+    language: number,
+    isSentence: boolean
 }
 
-const Words = async ({ query, filter, language }: Filters) => {
+const Words = async ({ query, filter, language, isSentence }: Filters) => {
     const words = await fetchAllWords(query, filter, language)
 
     return (
         <>
             <div className="flex items-center justify-between">
                 {/* mobile heading - only "tap" change*/}
-                <h3 className="md:hidden text-gray-700 max-sm:text-sm">
-                    {words.length < 1 ? 'No words found' : 'Tap to see translations and more'}
-                </h3>
+                {!isSentence && (
+                    <>
+                        <h3 className="md:hidden text-gray-700 max-sm:text-sm">
+                            {words.length < 1 ? 'No words found' : 'Tap to see translations and more'}
+                        </h3>
 
-                <h3 className="hidden md:block text-gray-700 max-sm:text-sm">
-                    {words.length < 1 ? 'No words found' : 'Hover to see translations and more'}
-                </h3>
+                        <h3 className="hidden md:block text-gray-700 max-sm:text-sm">
+                            {words.length < 1 ? 'No words found' : 'Hover to see translations and more'}
+                        </h3>
+                    </>
+                )}
             </div>
+
             <section className="flex flex-wrap max-lg:justify-between justify-center lg:grid grid-cols-8 items-center gap-4 sm:gap-8">
                 {words?.map((word: Word) => (
-                    <WordContainer key={word.id} word={word} />
+                    <WordContainer language={language} key={word.id} word={word} isSentence={isSentence} />
                 ))}
             </section>
         </>
