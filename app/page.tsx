c8d67896c10d3bbbe353670a96fe49a54801b156
jbresky <joaquin_bresky@hotmail.com>
2024-03-06 12:55:05 +0100

    modal searchParam prop & words async component

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,17 @@ import PostForm from "@/components/forms/post-form";
 import { getSession } from "@/server/session";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
+import Words from "./my-words/components/words";
 
 export default async function Home({
   searchParams
 }: {
-  searchParams: { language: string }
+  searchParams: { language: string, query?: string }
 }) {
   const session = await getSession()
 
   if (!session) redirect('/login')
+  const showModal = searchParams?.query
 
   const language = Number(searchParams?.language)
 
@@ -19,10 +21,14 @@ export default async function Home({
     <>
       <main className="my-4">
         <Suspense fallback={'Loading...'} >
-          {/* @ts-ignore Async Server Component */}
+           {/* @ts-ignore Async Server Component */}
           <LastWords language={language || 1} />
         </Suspense>
         <PostForm languageId={language || 1} />
+        {showModal && (
+          // @ts-ignore Async Server Component
+          <Words query={showModal} language={language || 1} isSentence={true} />
+        )}
       </main>
     </>
   );
